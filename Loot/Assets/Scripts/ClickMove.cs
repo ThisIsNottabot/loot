5a5f47c85401536abd4a6f8824cdbcdd24e7d234
nottabot <colonelalabasterunibrow@gmail.com>
2018-02-28 14:10:37 -0500

    Click to Move

---
+++ Loot/Assets/Scripts/ClickMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMove : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent agent;
+	public GameObject indicator;
+
+	void Awake() {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+
+	void Update() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown("Fire2")) {
+			if (Physics.Raycast(ray, out hit, 100)) {
+				agent.destination = hit.point;
+				agent.Resume();
+				indicator.transform.position = hit.point;
+			}
+		}
+	}
+}
